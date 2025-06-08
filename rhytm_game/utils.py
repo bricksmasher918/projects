c69e28f916e71a8a22f49a963a768e43a7851211
Dang Huy Loc <danghuyloc918@gmail.com>
2025-06-08 22:41:38 +0200

    Delete rhytm_game directory

--- rhytm_game/utils.py
+++
@@ -1,9 +0,0 @@
-def count_score(score, combo_multiplier):
-    if combo_multiplier > 0:
-        return score + int(100 * combo_multiplier)
-    return score + 100
-
-def display_score(screen, font, score):
-    text = font.render(f"Score: {score}", False, "white")
-    rect = text.get_rect(center=(125, 50))
-    screen.blit(text, rect)
